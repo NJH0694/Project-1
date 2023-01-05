8b048f3bb9cd549c40120158854fbf3b9ed58999
NJH0694 <118283777+NJH0694@users.noreply.github.com>
2023-01-05 18:01:33 +0800

    Update 02 Read.R

--- 02 Read.R
+++ 02 Read.R
@@ -1,6 +1,7 @@
 #Read raw data file name and create tidied file name
 file_raw <- paste0('Raw Data/',raw_list[i])
 file_tidy <- paste0('Tidy Data/', str_replace(raw_list[i],'xls','csv'))
+dir.create('Tidy Data', showWarnings = FALSE)
 
 #Read xls and convert to tibble (might takes few second)
 file_read <- as_tibble(read_xls(path=file_raw, range="A3:AZ70"))
